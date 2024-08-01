bcfc538bc9731d0d93066ea2119156a23da04dc8
Adam Sabbah <0x004d4m@gmail.com>
2024-08-01 15:04:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getPosts();
+  }
+
+  void getPosts() async {
+    var url = Uri.https('jsonplaceholder.typicode.com', '/posts');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = convert.jsonDecode(response.body);
+      setState(() {
+        _posts = jsonResponse.map((data) => Post.fromJson(data)).toList();
+      });
+    } else {
+      debugPrint('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: _posts.length,
+        itemBuilder: (context, index) {
+          final post = _posts[index];
+          return ListTile(
+            title: Text(post.title),
+            subtitle: Text(post.body),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Post {userId: $userId, id: $id, title: $title}';
+  }
+}
